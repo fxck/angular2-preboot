8eeed22a2ccea346370cba3dd970423e73a900b0
Döll, Sebastian <sebastian.doell@axelspringer.de>
2017-01-18 21:08:23 +0100

    ~ changing tslint defaults

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -8,6 +8,10 @@
 declare module '*';
 
 // Extra variables that live on Global that will be replaced by webpack DefinePlugin
-declare var ENV: string;
+declare var System: SystemJS;
 declare var __PROD__: boolean;
 declare var __DEV__: boolean;
+
+interface SystemJS {
+  import: (path?: string) => Promise<any>;
+}
